364d870a08f2bd56ade04d2ddfe8363ec492e7cb
dagmawikk <dagmawikassa1@gmail.com>
2025-04-05 23:10:26 +0300

    Implement courseinfo 1.3

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -32,9 +32,9 @@ const Content = ({info}) => {
   return (
     <div>
       {
-        info[0].map((p, idx) => 
+        info.map((p, idx) => 
         (
-          <Part key = {idx} partNum = {p} exerciseNumber = {info[1][idx]} />
+          <Part key = {idx} partNum = {p.name} exerciseNumber = {p.exercises} />
         )
         )
       } 
@@ -47,26 +47,32 @@ const Total = ({tot}) => {
   for (let i = 0; i < tot.length; i++) {
     total += tot[i]
   }
-  
+
   return (
     <p>Number of exercises {total}</p>
   )
 };
 
 const App = () => {
-  const course = 'Half Stack application development';
-  const part1 = 'Fundamentals of React';
-  const exercises1 = 10;
-  const part2 = 'Using props to pass data';
-  const exercises2 = 7;
-  const part3 = 'State of a component';
-  const exercises3 = 14;
+  const course = 'Half Stack application development'
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  }
 
   return (
     <div>
       <Header course={course} />
-      <Content info = {[[part1, part2, part3], [exercises1, exercises2, exercises3]]} />      
-      <Total tot = {[exercises1, exercises2, exercises3]} />
+      <Content info = {[part1, part2, part3]} />      
+      <Total tot = {[part1.exercises, part2.exercises, part3.exercises]} />
     </div>
   )
 };
