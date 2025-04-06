99ea7ea7e70cb44797cc2265e946ed74c34c7ac2
dagmawikk <dagmawikassa1@gmail.com>
2025-04-07 01:14:28 +0300

    Implement phonebook 2.10

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,5 +1,44 @@
 import { useState } from 'react'
 
+const Search = ({searchSubmit, handleInputSearch, search}) => {
+  return (
+    <form onSubmit={searchSubmit}>
+    <input onChange={handleInputSearch}type="text" name="search" id="search" value={search}/>
+  </form>
+  )
+}
+
+const AddNewPerson = ({handleSubmit, handleNewName, newName, handleNewNumber, newNumber}) => {
+  return (
+    <form onSubmit={handleSubmit}>
+    <div>
+      <label htmlFor="name">name: </label>
+      <input required onChange={handleNewName} value={newName} type="text" name="name" id="name" />
+    </div>
+    <div>
+      <label htmlFor="number">number: </label>
+      <input required onChange={handleNewNumber} value={newNumber} type="text" name="number" id="number" />
+    </div>
+    <div>
+      <button type='submit'>add</button>
+    </div>
+  </form>
+  )
+}
+
+const Person = ({person}) => {
+  return (
+    <p>{person.name} {person.number}</p>
+  )
+}
+
+const DisplayNumbers = ({filtered}) => {
+  return (
+    <>
+    {filtered.map((person, idx) => <Person key={idx} person={person}/>)}
+    </>
+  )
+}
 const App = () => {
   const [persons, setPersons] = useState([
     { name: 'Arto Hellas', number: '040-123456', id: 1 },
@@ -7,33 +46,25 @@ const App = () => {
     { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
     { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
   ])
-
   const [newName, setNewName] = useState("")
   const [newNumber, setNewNumber] = useState("")
   const [search, setSearch] = useState("")
   const [filtered, setFiltered] = useState(persons)
+
+  const handleNewName = (e) => setNewName(e.target.value)
+  const handleNewNumber = (e) => setNewNumber(e.target.value)
+  const handleInputSearch = (e) => setSearch(e.target.value)
 
   const handleSubmit = (e) => {
     e.preventDefault()
     const alreadyAdded = persons.find((person) => person.name === newName)
     if (alreadyAdded === undefined) {
       setPersons(persons.concat({name: newName, number: newNumber}))
-      setFiltered(persons)
+      setFiltered(filtered.concat({name: newName, number: newNumber}))
     }
     else {
       alert(`${newName} is already added to phonebook`)
     }
-  }
-
-  const handleNewName = (e) => {
-    setNewName(e.target.value)
-  }
-  const handleNewNumber = (e) => {
-    setNewNumber(e.target.value)
-  }
-
-  const handleInputSearch = (e) => {
-    setSearch(e.target.value)
   }
 
   const searchSubmit = (e) => {
@@ -46,29 +77,16 @@ const App = () => {
       setFiltered(persons)
     }
   }
+
   return (
     <div>
       <h2>Phonebook</h2>
-        <form onSubmit={searchSubmit}>
-          <input onChange={handleInputSearch}type="text" name="search" id="search" value={search}/>
-        </form>
+      <Search searchSubmit={searchSubmit} handleInputSearch={handleInputSearch} search = {search}/>
       <h2>add a new</h2>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label htmlFor="name">name: </label>
-          <input required onChange={handleNewName} value={newName} type="text" name="name" id="name" />
-        </div>
-        <div>
-          <label htmlFor="number">number: </label>
-          <input required onChange={handleNewNumber} value={newNumber} type="text" name="number" id="number" />
-        </div>
-        <div>
-          <button type='submit'>add</button>
-        </div>
-      </form>
+      <AddNewPerson handleSubmit={handleSubmit} handleNewName={handleNewName} newName={newName} handleNewNumber={handleNewNumber} newNumber={newNumber}/>
       <div>
         <h2>Numbers</h2>
-        {filtered.map((person, idx) => <p key={idx}>{person.name} {person.number}</p>)}
+        <DisplayNumbers filtered={filtered}/>
       </div>
     </div>
   )
