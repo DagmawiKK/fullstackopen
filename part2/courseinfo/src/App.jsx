c44a2a0fe5bb47be59edfe76e535bb6d1c0ec424
dagmawikk <dagmawikassa1@gmail.com>
2025-04-06 20:50:03 +0300

    Implement courseinfo 2.4

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -29,11 +29,8 @@ const Content = ({info}) => {
 }
 
 const Total = ({tot}) => {
-  let total = tot.reduce((accu, val) => {
-    console.log(val)
-    return accu + val.exercises
-  }, 0,)
-  console.log(tot)
+  let total = tot.reduce((accu, val) => accu + val.exercises, 0)
+
   return (
     <p>Number of exercises {total}</p>
   )
@@ -50,36 +47,58 @@ const Course = ({course}) => {
 }
 
 const App = () => {
-  const course = {
-    id: 1,
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10,
-        id: 1
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7,
-        id: 2
-      },
-      {
-        name: 'State of a component',
-        exercises: 14,
-        id: 3
-      },
-      {
-        name: "Redux",
-        exercises: 11,
-        id: 4
-      }
-    ]
-  }
+  const courses = [
+    {
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4
+        }
+      ]
+    }, 
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
+  ]
 
   return (
     <div>
-      <Course course = {course}/>     
+      {
+        courses.map(
+          (course) => <Course course = {course}/>
+        )
+      } 
     </div>
   )
 };
