e0d17a9a60f608d5a957885d1cc2bca42c82a188
dagmawikk <dagmawikassa1@gmail.com>
2025-04-06 14:52:56 +0300

    Implement unicafe 1.9

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -19,20 +19,29 @@ const GiveFeedback = ({handleFeedback}) => {
 }
 
 const Statistics = ({good, neutral, bad}) => {
-  const total = good + neutral + bad  ?? 0
-  const avg = Number.isNaN((good - bad) / total) ? 0: (good - bad) / total
-  const pos = Number.isNaN(good / total) ? 0 : good / total;
-  return (
-    <div>
-      <h1>statistics</h1>
-      <p>good {good}</p>
-      <p>neutral {neutral}</p>
-      <p>bad {bad}</p>
-      <p>all {total}</p>
-      <p>average {avg}</p>
-      <p>positive {pos}</p>
-    </div>
-  )
+  if (good == 0 && neutral == 0 && bad == 0) {
+    return(
+      <div>
+        <h1>statistics</h1>
+        <p>No feedback given yets</p>
+      </div>
+    )
+  }
+  else {
+    const total = good + neutral + bad  ?? 0
+    const avg = Number.isNaN((good - bad) / total) ? 0: (good - bad) / total
+    const pos = Number.isNaN(good / total) ? 0 : good / total;
+    return (
+      <div>
+        <h1>statistics</h1>
+        <p>good {good}</p>
+        <p>neutral {neutral}</p>
+        <p>bad {bad}</p>
+        <p>all {total}</p>
+        <p>average {avg}</p>
+        <p>positive {pos}</p>
+      </div>
+    )}
 }
 
 const App = () => {
